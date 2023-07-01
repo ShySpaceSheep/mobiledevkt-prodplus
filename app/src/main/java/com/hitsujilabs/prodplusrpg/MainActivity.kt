d4fcf72437a9d068fa444e088d0a6971ea1920a6
ShySpaceSheep <theminecraftf303@gmail.com>
2023-07-01 16:02:50 +0800

    07/01/23 - 16:00 * Continued porting Java files to Kotlin files - There is currently a fatal error involving FragmentContainer, this will be fixed soon, hopefully.

--- app/src/main/java/com/hitsujilabs/prodplusrpg/MainActivity.kt
+++ app/src/main/java/com/hitsujilabs/prodplusrpg/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.hitsujilabs.prodplusrpg
 
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.ActionBar
 import android.os.Bundle
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var bottomNavView : BottomNavigationView
+    private lateinit var sidebarNavView : DrawerLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Set action bar display here by modifying the action bar object:
+        val actionBar : ActionBar? = supportActionBar
+        actionBar?.title = "ProdPlus-RPG"
+        actionBar?.subtitle = "You currently have 0 tasks!"
     }
 }
---
+++ app/src/main/java/com/hitsujilabs/prodplusrpg/NavFragments/NotesFragment.kt
@@ -0,0 +1,4 @@
+package com.hitsujilabs.prodplusrpg.NavFragments
+
+class NotesFragment {
+}
